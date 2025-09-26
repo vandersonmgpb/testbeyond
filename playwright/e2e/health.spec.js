d72902b1e05e74c115d5db3c78fc2905edff8380
vandersonmgpb <vanderson@gmail.com>
2025-09-26 08:47:18 -0300

    automação de testes com playwright de um encurtador de links

---
+++ playwright/e2e/health.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test'
+
+test('deve verificar se a api está online', async ({ request }) => {
+  
+  const response = await request.get('http://localhost:3333/health')
+
+  expect(response.status()).toBe(200) 
+
+  const body = await response.json()
+  expect(body.service).toBe('shortbeyond-api')
+  expect(body.status).toBe('healthy')
+
+})
+
+
